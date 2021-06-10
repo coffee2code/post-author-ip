40938a0c62976fb2d464651a8960bd7a5e896f0f
Scott Reilly <scott@coffee2code.com>
2021-06-09 23:12:47 -0700

    Remove deprecated JS files

--- assets/js/src/editor.js
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-import { default as PostAuthorIP } from './components/post-author-ip';
-
-registerPlugin(
-	'post-author-ip',
-	{
-		render: PostAuthorIP,
-	}
-);
